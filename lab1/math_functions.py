ab4e5b0a1c3569a9a22b45cac8c276c15473d463
cdrainkainos <christy.drain@kainos.com>
2025-02-18 08:50:13 +0000

    fixed add method

--- lab1/math_functions.py
+++ lab1/math_functions.py
@@ -1,5 +1,5 @@
 def add(x, y):
     if isinstance(x, int) or isinstance(y, int):
-        return "At least one number is non-numeric"
-    else:
         return x + y
+    else:
+        return "At least one number is non-numeric"
